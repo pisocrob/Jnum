41005abc1beee773e66be9da62e604a446f3b920
pisocrob <r.e.norvill@student.bradford.ac.uk>
2016-12-06 15:23:07 +0000

    add random generation functionality

--- Jnum.py
+++ Jnum.py
@@ -1,9 +1,5 @@
 #coding=utf-8
-
-
-
-
-#romanNumList = raw_input("Numbers: ").split(',')
+import random
 
 
 jSingles = {0: '零', 1: '一', 2: '二', 3: '三', 4: '四', 5: 'ご', 6: '六', 7: '七', 8: '八', 9: '九'}
@@ -14,22 +10,33 @@ def parsetoJNumeric(euroNum):
     jNum = ''
     i = 0;
     num = int(euroNum)
+    num = euroNum
     while (num != 0):
         digit = num % 10
         num = num / 10
         if digit != 0:
             jNum = jSingles[digit] + jTens[i] + jNum
         i += 1
-
     return jNum
 
-r = raw_input("Number: ")
+def genRandomJNums(r):
+    total = r.split(" ")
+    rangeMin = int(total[1])
+    rangeMax = int(total[2])
+    i = int(total[3])
+    while i > 0:
+        print parsetoJNumeric(random.randint(rangeMin, rangeMax))
+        i -= 1
 
+r = raw_input("Number: ")
 if r.find(",") == -1:
-    print parsetoJNumeric(r)
+    if r.find("rand") > -1:
+        genRandom(r)
+    else:
+        print parsetoJNumeric(r)
+
 else:
     euroNumList = r.split(",")
     for num in euroNumList:
-
         print num + ': ' + parsetoJNumeric(num)
 
--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Parser for Japanese Numbers
 Simple parser which takes a European number as an input and outputs the Japanese equivalent. Works up to the 10,000's.
 
 # Usage
-Input takes the form of a single European number or mutliple numbers seperated by a comma.
+Input takes the form of a single European number or mutliple numbers seperated by a commas.
+Will output a given number of Japanese numbers in a given range when the input takes the format:
+rand <min> <max> <amount>
