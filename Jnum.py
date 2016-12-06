49dcdc2fa7178056e142b6bb9994ad19e076902a
pisocrob <r.e.norvill@student.bradford.ac.uk>
2016-12-06 13:04:20 +0000

    add multiple input cuntionality. Update readme

--- Jnum.py
+++ Jnum.py
@@ -1,19 +1,19 @@
 #coding=utf-8
-import string
+
 
 
 
 #romanNumList = raw_input("Numbers: ").split(',')
-r = raw_input("Number: ")
+
 
 jSingles = {0: '零', 1: '一', 2: '二', 3: '三', 4: '四', 5: 'ご', 6: '六', 7: '七', 8: '八', 9: '九'}
 jTens = ['', '十', '百', '千', '万']
 
 
-def ParsetoJNumeric(romanNum):
+def parsetoJNumeric(euroNum):
     jNum = ''
     i = 0;
-    num = int(romanNum)
+    num = int(euroNum)
     while (num != 0):
         digit = num % 10
         num = num / 10
@@ -23,4 +23,13 @@ def ParsetoJNumeric(romanNum):
 
     return jNum
 
-print ParsetoJNumeric(r)
+r = raw_input("Number: ")
+
+if r.find(",") == -1:
+    print parsetoJNumeric(r)
+else:
+    euroNumList = r.split(",")
+    for num in euroNumList:
+
+        print num + ': ' + parsetoJNumeric(num)
+
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Jnum
-Parser for Japanes Numbers
+Parser for Japanese Numbers
 
-Simple parser which takes a European number as an input and outputs the Japanese equivalent.
+Simple parser which takes a European number as an input and outputs the Japanese equivalent. Works up to the 10,000's.
+
+# Usage
+Input takes the form of a single European number or mutliple numbers seperated by a comma.
